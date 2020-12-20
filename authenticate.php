04019c93b87fe354fb331d6896ca511af8111926
hjoaquim <h.joaquim@campus.fct.unl.pt>
2020-12-20 15:43:31 +0000

    check if user exists in db

---
+++ authenticate.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'sim';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME) or die('Failed to connect to MySQL: ' . mysqli_error($connect));
+
+
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
+
+$query = 'SELECT * FROM utl_utilizadores WHERE username = "'.$_POST['username'].'"';
+$result = mysqli_query($con, $query) or die('The query failed: ' . mysqli_error($con));
+
+if (mysqli_num_rows($result) > 0) {
+
+	$user = mysqli_fetch_row($result);
+	// Account exists, now we verify the password.
+	// Note: remember to use password_hash in your registration file to store the hashed passwords.
+	if (password_verify($_POST['password'], $user[2])) {
+		// Verification success! User has loggedin!
+		// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+		session_regenerate_id();
+		$_SESSION['loggedin'] = TRUE;
+		$_SESSION['name'] = $user[1];
+		$_SESSION['id'] = $user[0];
+		echo 'Welcome ' . $_SESSION['name'] . '!';
+	} else {
+		// Incorrect password
+		echo 'Incorrect username and/or password!';
+	}
+} else {
+	// Incorrect username
+	echo 'Incorrect username and/or password!';
+}
+
+
+?>
+
