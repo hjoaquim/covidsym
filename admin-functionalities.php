42f2419c4c8c6e49d6c0761ea71dec8f9c934ba2
hjoaquim <h.joaquim@campus.fct.unl.pt>
2021-01-09 12:26:11 +0000

    change user almost

--- admin-functionalities.php
+++ admin-functionalities.php
@@ -4,7 +4,7 @@
     }
 
     if (!isset($_SESSION['loggedin'], $_SESSION['usr_type']) && $_SESSION['usr_type']!='admin') {
-        header('Location: index.html');
+        header('Location: index.php');
         exit;
     }
 
@@ -113,13 +113,157 @@ function search_user($con){
     echo ('
 
     <div>
-        <form action="admin-functionalities.php?op=3">
+        <form action="admin-functionalities.php">
             <input type="text" placeholder="Search by username" name="search">
+            <input type="hidden" name="op" value="3">
             <button type="submit"><i class="fa fa-search"></i></button>
         </form>
     </div>
 
     ');
+
+    
+
+    if(isset($_GET['search'])){
+
+        $query = 'SELECT * FROM utl_utilizadores WHERE username = "'.$_GET['search'].'"';
+        $result = mysqli_query($con, $query) or die('The query failed: ' . mysqli_error($con));
+
+
+
+        if (mysqli_num_rows($result) > 0) {
+            
+            $user = mysqli_fetch_row($result);
+
+            $query = 'SELECT NOME_DISTRITO FROM LOC_DISTRITOS WHERE COD_DISTRITO = '.$user[6].';';
+            $result = mysqli_query($con, $query) or die('The query failed: ' . mysqli_error($con));
+            $dist = mysqli_fetch_row($result);
+
+            $query = 'SELECT DESC_UTL FROM UTL_TIPOS_UTILIZADORES WHERE ID='.$user[13].';';
+            $result = mysqli_query($con, $query) or die('The query failed: ' . mysqli_error($con));
+            $tipo = mysqli_fetch_row($result);
+
+            $query = 'SELECT nome_distrito FROM loc_distritos';
+            $result_dist = mysqli_query($con, $query) or die('The query failed: ' . mysqli_error($con));
+
+            $optionString = '';
+            while($row = mysqli_fetch_array($result_dist)){
+                $optionString .='<option>'.$row["nome_distrito"].'</option>';
+            }
+
+            mysqli_close($con);
+
+            echo('
+            
+                    <table>
+                    <form action="change-user.php" method="post"  enctype="multipart/form-data">
+                                <tr>                                
+                                    
+                                    <td>
+                                    <div class="pricing-entry pb-5 text-center">
+                                            
+                                                <table>
+                                                    <tr>
+                                                        <td>Choose Image:</td>
+                                                        <td><input type="file" id="myimage" name="myimage"></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Username:</td>
+                                                        <td>'.$user[1].'</td>
+                                                    </tr>
+
+                                                    <tr>
+                                                        <td>Nome:</td>
+                                                        <td><input type="text" name="name" value="'.$user[3].'" style="width: 300px"></td>
+                                                    <tr>
+
+                                                    <tr>
+                                                        <td>Password: <input type="checkbox" onclick="showpw()"></td>
+                                                        <td>
+                                                            <input type="password" name="password" style="width: 300px" id=myPw>
+                                                        </td>
+                                                        
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Morada:</td>
+                                                        <td>
+                                                            <input type="text" name="address" value="'.$user[5].'" style="width: 300px">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Distrito:</td>
+                                                        <td>
+                                                        <select name="district" id="district" style="width: 300px">
+                                                        <option><?=$dist[0]?></option>
+                                                        '.$optionString.'
+                                                        </select>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Telemóvel:</td>
+                                                        <td>
+                                                            <input type="tel" name="contact" value="'.$user[7].'" id="contact" style="width: 300px">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Email:</td>
+                                                        <td>
+                                                            <input type="email" name="email" value="'.$user[8].'" id="email" style="width: 300px">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Data de Nascimento:</td>
+                                                        <td>
+                                                            <input type="date" name="birthdate" value="'.$user[9].'" id="birthdate" style="width: 300px">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Nº cartão de utente:</td>
+                                                        <td>
+                                                            <input type="number" name="health-card-number" value="'.$user[10].'" id="health-card-number" style="width: 300px">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>NIF:</td>
+                                                        <td>
+                                                            <input type="number" name="nif" value="'.$user[11].'" id="nif" style="width: 300px">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Genero:</td>
+                                                        <td>
+                                                            <select name="gender" id="gender" style="width: 300px">
+                                                            <option value="male">Male</option>
+                                                            <option value="female">Female</option>
+                                                            </select>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Tipo de utilizador:</td>
+                                                        <td><'.$tipo[0].'</td>
+                                                    </tr>
+
+                                                    <tr><td><td> <input type="submit" value="Save changes"> <td></td></tr>
+
+                                                </table>
+                                            </div>
+                                    </td>
+
+                                </tr>
+                </form>
+                </table>
+
+            ');
+
+        }
+        else{
+            $message = "User not found";
+            echo "<SCRIPT>alert('$message'); window.location.replace('admin-functionalities.php?op=3');</SCRIPT>";
+        }
+
+        
+    }
+    
 
     echo "</div>";
     echo '</section>';
